a71500319e9b6526b638c77497df1db38fb8abe2
vishwasraiborde19 <vishwas.raiborde@gmail.com>
2025-01-30 22:25:47 +0000

    jenkins

---
+++ platform-discovery-server/src/test/java/com/platform/platformdiscoveryservice/PlatformDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.platformdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
