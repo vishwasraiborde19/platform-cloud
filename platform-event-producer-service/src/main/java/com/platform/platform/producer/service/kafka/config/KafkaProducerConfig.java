fbcd093af07ab44a52ae4b51cac0886c41f0830d
vishwasraiborde19 <vishwas.raiborde@gmail.com>
2025-02-19 15:06:27 +0000

    kafka producer and consumer  added

--- platform-event-producer-service/src/main/java/com/platform/platform/producer/service/kafka/config/KafkaProducerConfig.java
+++ platform-event-producer-service/src/main/java/com/platform/platform/producer/service/kafka/config/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ public class KafkaProducerConfig {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.87:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-node1:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.ACKS_CONFIG, "all");
