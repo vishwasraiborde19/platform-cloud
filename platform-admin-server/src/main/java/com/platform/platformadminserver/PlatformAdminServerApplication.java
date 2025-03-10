106da7d3583134bfff4e75b2ae7ffa1d7b0d4629
vishwasraiborde19 <vishwas.raiborde@gmail.com>
2025-03-10 23:39:30 +0000

    eureka and admin changes

--- platform-admin-server/src/main/java/com/platform/platformadminserver/PlatformAdminServerApplication.java
+++ platform-admin-server/src/main/java/com/platform/platformadminserver/PlatformAdminServerApplication.java
@@ -3,9 +3,11 @@ package com.platform.platformadminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class PlatformAdminServerApplication {
 
     public static void main(String[] args) {
