2e9723bbca0da63d556c67dd6b563153af50a6f9
vishwasraiborde19 <vishwas.raiborde@gmail.com>
2025-04-04 13:42:22 +0100

    functional bits added

---
+++ lms-user-mgmt-service/src/test/java/com/lms/mgm/UserDTOMgmtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.mgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDTOMgmtServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
