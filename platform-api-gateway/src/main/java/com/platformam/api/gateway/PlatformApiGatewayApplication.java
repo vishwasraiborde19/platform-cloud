ade5391fd7437a26083fdc03d7b0bb05ff3de8a4
vishwasraiborde19 <vishwas.raiborde@gmail.com>
2025-04-14 00:57:51 +0100

    logging and exceptions

---
+++ platform-api-gateway/src/main/java/com/platformam/api/gateway/PlatformApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.platformam.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApiGatewayApplication.class, args);
+    }
+
+}
---
+++ platform-api-gateway/src/test/java/com/platformam/api/gateway/PlatformApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platformam.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
